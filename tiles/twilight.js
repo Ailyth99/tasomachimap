e4df6df8909f98c52d4987d976ecfa2f658db5c3
akk1921 <aikikarius@gmail.com>
2022-05-05 13:27:45 +0800

    语法调整，；

--- tiles/twilight.js
+++ tiles/twilight.js
@@ -1,12 +1,4 @@
-function sliderX() {
-	document.getElementById('side').classList.toggle("panelHidden"),
-	document.getElementById('slider').classList.toggle("slider")
-}
 
-function sliderM() {
-	document.getElementById('side').classList.toggle("panelHiddenM"),
-	document.getElementById('sliderM').classList.toggle("sliderMx")
-}
 
 var SW = L.latLng( - 305, -120),
 NE = L.latLng(135, 487),
@@ -27,15 +19,15 @@ var map = L.map('lmap', {
 var opt = {
 	tileSize: L.point(192, 192)
 };
-shiokaze = L.tileLayer('shiokaze/tiles/{z}/{x}/{y}.webp', opt);
-futago = L.tileLayer('futago/tiles/{z}/{x}/{y}.webp', opt);
-gengetsu = L.tileLayer('gengetsu/tiles/{z}/{x}/{y}.webp', opt);
+shiokaze = L.tileLayer('shiokaze/tiles/{z}/{x}/{y}.webp', opt)
+futago = L.tileLayer('futago/tiles/{z}/{x}/{y}.webp', opt)
+gengetsu = L.tileLayer('gengetsu/tiles/{z}/{x}/{y}.webp', opt)
 
-shiokaze.addTo(map);
+shiokaze.addTo(map)
 
-map.setView([ - 93, 135], 2);
+map.setView([ - 93, 135], 2)
 
-map.zoomControl.setPosition('bottomleft');
+map.zoomControl.setPosition('bottomleft')
 
 var customPopup = {
 	'maxWidth': '300',
@@ -43,95 +35,95 @@ var customPopup = {
 },
  contentPopup = "";
 
-var shioKensaku = new L.layerGroup(), 
-futaKensaku = new L.layerGroup(),
-genKensaku = new L.layerGroup(),
-mkS = 40,
-mkC = mkS / 2 - 3.5,
-mkP = -mkS - 2,
-shioMKe = [],
-shioMKf = [],
-shioMKc = [],
-shioMKn = [],
-shioMKco = [],
-futaMKe = [],
-futaMKf = [],
-futaMKc = [],
-futaMKn = [],
-futaMKco = [],
-genMKe = [],
-genMKf = [],
-genMKn = [],
-genMKco = [],
+var shioKensaku = new L.layerGroup()
+futaKensaku = new L.layerGroup()
+genKensaku = new L.layerGroup()
+mkS = 40
+mkC = mkS / 2 - 3.5
+mkP = -mkS - 2
+shioMKe = []
+shioMKf = []
+shioMKc = []
+shioMKn = []
+shioMKco = []
+futaMKe = []
+futaMKf = []
+futaMKc = []
+futaMKn = []
+futaMKco = []
+genMKe = []
+genMKf = []
+genMKn = []
+genMKco = []
 genMKc = [];
 
 for (i = 0; i < itemData.shioItems.length; i++) {
-	let j = itemData.shioItems[i],
+	let j = itemData.shioItems[i]
 	icon = L.icon({
 		iconUrl: 'tiles/Sprite/icon/' + j.locationCategory + '.png',
 		iconSize: [40, ],
 		iconAnchor: [mkC, mkS],
 		shadowSize: [0, 0],
 		popupAnchor: [0, mkP],
-	});
+	})
 
 	let mkShio = new L.marker(new L.latLng([ - j.h, j.s]), {
 		icon: icon,
 		title: j.name
-	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup);
-	shioKensaku.addLayer(mkShio);
+	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup)
+	shioKensaku.addLayer(mkShio)
 
 	switch (j.locationCategory) {
 	case "shio-earth-src":
-		shioMKe.push(mkShio);
+		shioMKe.push(mkShio)
 		break;
 	case "shio-fur":
-		shioMKf.push(mkShio);
+		shioMKf.push(mkShio)
 		break;
 	case "shio-costume":
-		shioMKc.push(mkShio);
+		shioMKc.push(mkShio)
 		break;
 	case "shio-note":
-		shioMKn.push(mkShio);
+		shioMKn.push(mkShio)
 		break;
 	case "shio-corrupt":
-		shioMKco.push(mkShio);
+		shioMKco.push(mkShio)
 		break;
 	}
 }
 
 for (i = 0; i < itemData.futaItems.length; i++) {
-	let j = itemData.futaItems[i],
+	let j = itemData.futaItems[i]
 	icon = L.icon({
 		iconUrl: 'tiles/Sprite/icon/' + j.locationCategory + '.png',
 		iconSize: [40, ],
 		iconAnchor: [mkC, mkS],
 		shadowSize: [0, 0],
 		popupAnchor: [0, mkP],
-	});
+	})
 
 	let mkFuta = new L.marker(new L.latLng([ - j.h, j.s]), {
 		icon: icon,
 		title: j.name
-	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup);
-	futaKensaku.addLayer(mkFuta);
+	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup)
+	futaKensaku.addLayer(mkFuta)
 
 	switch (j.locationCategory) {
 
 	case "futa-earth-src":
-		futaMKe.push(mkFuta);
+		futaMKe.push(mkFuta)
 		break;
 	case "futa-fur":
-		futaMKf.push(mkFuta);
+		futaMKf.push(mkFuta)
 		break;
 	case "futa-costume":
-		futaMKc.push(mkFuta);
+		futaMKc.push(mkFuta)
 		break;
 	case "futa-note":
-		futaMKn.push(mkFuta);
+		futaMKn.push(mkFuta)
 		break;
 	case "futa-corrupt":
-		futaMKco.push(mkFuta);
+		futaMKco.push(mkFuta)
 		break;
 
 	}
@@ -150,48 +142,48 @@ for (i = 0; i < itemData.genItems.length; i++) {
 	let mkGen = new L.marker(new L.latLng([ - j.h, j.s]), {
 		icon: icon,
 		title: j.name
-	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup);
+	}).bindPopup("<h3>" + j.name + "</h3><br>" + j.content, customPopup)
 	genKensaku.addLayer(mkGen);
 
 	switch (j.locationCategory) {
 	case "gen-earth-src":
-		genMKe.push(mkGen);
+		genMKe.push(mkGen)
 		break;
 	case "gen-fur":
-		genMKf.push(mkGen);
+		genMKf.push(mkGen)
 		break;
 	case "gen-costume":
-		genMKc.push(mkGen);
+		genMKc.push(mkGen)
 		break;
 	case "gen-note":
-		genMKn.push(mkGen);
+		genMKn.push(mkGen)
 		break;
 	case "gen-corrupt":
-		genMKco.push(mkGen);
+		genMKco.push(mkGen)
 		break;
 
 	}
 }
 
-var she = L.layerGroup(shioMKe),
-shc = L.layerGroup(shioMKc),
-shf = L.layerGroup(shioMKf),
-shn = L.layerGroup(shioMKn),
-sho = L.layerGroup(shioMKco),
-sh = [she, shc, shf, shn, sho],
+var she = L.layerGroup(shioMKe)
+shc = L.layerGroup(shioMKc)
+shf = L.layerGroup(shioMKf)
+shn = L.layerGroup(shioMKn)
+sho = L.layerGroup(shioMKco)
+sh = [she, shc, shf, shn, sho]
 
-gee = L.layerGroup(genMKe),
-gec = L.layerGroup(genMKc),
-gef = L.layerGroup(genMKf),
-gen = L.layerGroup(genMKn),
-geo = L.layerGroup(genMKco),
-ge = [gee, gec, gef, gen, geo],
+gee = L.layerGroup(genMKe)
+gec = L.layerGroup(genMKc)
+gef = L.layerGroup(genMKf)
+gen = L.layerGroup(genMKn)
+geo = L.layerGroup(genMKco)
+ge = [gee, gec, gef, gen, geo]
 
-fue = L.layerGroup(futaMKe),
-fuc = L.layerGroup(futaMKc),
-fun = L.layerGroup(futaMKn),
-fuo = L.layerGroup(futaMKco),
-fuf = L.layerGroup(futaMKf),
+fue = L.layerGroup(futaMKe)
+fuc = L.layerGroup(futaMKc)
+fun = L.layerGroup(futaMKn)
+fuo = L.layerGroup(futaMKco)
+fuf = L.layerGroup(futaMKf)
 fu = [fue, fuc, fun, fuo, fuf];
 
 function sideSwiper() {
@@ -218,8 +210,8 @@ var shioKensakuCtrl = new L.Control.Search({
 map.addControl(shioKensakuCtrl);
 shioKensakuCtrl.on('search:locationfound',
 function(e) {
-	e.layer.openPopup();
-	sideSwiper();
+	e.layer.openPopup()
+	sideSwiper()
 
 });
 var futaKensakuCtrl = new L.Control.Search({
@@ -236,8 +228,8 @@ var futaKensakuCtrl = new L.Control.Search({
 })
 futaKensakuCtrl.on('search:locationfound',
 function(e) {
-	e.layer.openPopup();
-	sideSwiper();
+	e.layer.openPopup()
+	sideSwiper()
 });
 
 var genKensakuCtrl = new L.Control.Search({
@@ -300,64 +292,63 @@ function sweeper(x) {
 }
 
 function mapShow(x) {
-	let s = document.querySelector("#groupShio"),
-	f = document.querySelector("#groupFu"),
-	g = document.querySelector("#groupGen"),
-	sx = document.getElementsByName('shiokaze'),
-	fx = document.getElementsByName('futago'),
-	gx = document.getElementsByName('gen'),
-	sy = document.getElementsByName('shiokazeSQ'),
-	fy = document.getElementsByName('futagoSQ'),
-	gy = document.getElementsByName('genSQ');
+	let s = document.querySelector("#groupShio")
+	f = document.querySelector("#groupFu")
+	g = document.querySelector("#groupGen")
+	sx = document.getElementsByName('shiokaze')
+	fx = document.getElementsByName('futago')
+	gx = document.getElementsByName('gen')
+	sy = document.getElementsByName('shiokazeSQ')
+	fy = document.getElementsByName('futagoSQ')
+	gy = document.getElementsByName('genSQ')
 
 	switch (x) {
 	case "shio":
-		sweeper('shio');
+		sweeper('shio')
 
-		s.setAttribute('style', 'display:block');
-		f.setAttribute('style', 'display:none');
-		g.setAttribute('style', 'display:none');
+		s.setAttribute('style', 'display:block')
+		f.setAttribute('style', 'display:none')
+		g.setAttribute('style', 'display:none')
 		for (u = 0; u < sx.length; u++) {
-			sx[u].setAttribute("value", "show");
-			sx[u].classList.remove('transprt');
+			sx[u].setAttribute("value", "show")
+			sx[u].classList.remove('transprt')
 			sy[u].classList.remove('displayNone')
-		};
+		}
 
-		kensakuSweeper();
-		map.addControl(shioKensakuCtrl);
+		kensakuSweeper()
+		map.addControl(shioKensakuCtrl)
 
 		break;
 
 	case "futa":
 
-		sweeper('futa');
+		sweeper('futa')
 
-		s.setAttribute('style', 'display:none');
-		f.setAttribute('style', 'display:block');
-		g.setAttribute('style', 'display:none');
+		s.setAttribute('style', 'display:none')
+		f.setAttribute('style', 'display:block')
+		g.setAttribute('style', 'display:none')
 		for (u = 0; u < fx.length; u++) {
-			fx[u].setAttribute("value", "show");
-			fx[u].classList.remove('transprt');
+			fx[u].setAttribute("value", "show")
+			fx[u].classList.remove('transprt')
 			fy[u].classList.remove('displayNone')
-		};
+		}
 
-		kensakuSweeper();
-		map.addControl(futaKensakuCtrl);
-
+		kensakuSweeper()
+		map.addControl(futaKensakuCtrl)
 		break;
 
 	case "gen":
 		sweeper('gen');
-		s.setAttribute('style', 'display:none');
-		f.setAttribute('style', 'display:none');
-		g.setAttribute('style', 'display:block');
+		s.setAttribute('style', 'display:none')
+		f.setAttribute('style', 'display:none')
+		g.setAttribute('style', 'display:block')
 		for (u = 0; u < gx.length; u++) {
-			gx[u].setAttribute("value", "show");
-			gx[u].classList.remove('transprt');
+			gx[u].setAttribute("value", "show")
+			gx[u].classList.remove('transprt')
 			gy[u].classList.remove('displayNone')
-		};
-		kensakuSweeper();
-		map.addControl(genKensakuCtrl);
+		}
+		kensakuSweeper()
+		map.addControl(genKensakuCtrl)
 		break;
 	}
 }
@@ -372,17 +363,17 @@ function henkanki(z) {
 
 	switch (x) {
 	case "hidden":
-		y.addTo(map);
-		xa.setAttribute("value", "show");
-		xb.classList.remove('displayNone');
-		xa.classList.remove('transprt');
+		y.addTo(map)
+		xa.setAttribute("value", "show")
+		xb.classList.remove('displayNone')
+		xa.classList.remove('transprt')
 		break;
 
 	case "show":
-		y.removeFrom(map);
-		xa.setAttribute("value", "hidden");
-		xa.classList.add('transprt');
-		xb.classList.add('displayNone');
+		y.removeFrom(map)
+		xa.setAttribute("value", "hidden")
+		xa.classList.add('transprt')
+		xb.classList.add('displayNone')
 		break;
 	}
 }
